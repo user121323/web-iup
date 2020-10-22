a98815d3f050e119dfad5610900572413afca7c6
notNullnot <mr.zhandos00@list.ru>
2020-10-22 09:12:10 +0600

    urls.php

---
+++ urls.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>URLS</title>
+</head>
+<body>
+	<?php
+	echo "<a href='/adminPageListOfRequests.html'>adminPageListOfRequests</a><br>";
+	echo "<a href='/advisor.html'>advisor</a><br>";
+	echo "<a href='/rector.html'>rector</a><br>";
+	echo "<a href='/manager.html'>manager</a><br>";
+	echo "<a href='/mainPage.html'>mainPage</a><br>";
+	echo "<a href='/login.html'>login</a><br>";
+	echo "<a href='/registration.html'>registration</a><br>";
+	echo "<a href='/contacts.html'>contacts</a>";
+	?>	
+</body>
+</html>
